8f68eebcd5eb07433f7cb12e26cd775ac98e835d
Hoodie <hoodie.im@groundx.xyz>
2021-01-18 23:53:16 +0900

    Change style of header. Add logo click. Change water image

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ window.addEventListener('DOMContentLoaded', function(){
   const header = document.querySelector('.header')
   const menus = document.querySelectorAll('.menus')
   const bgs = document.querySelectorAll('.bg')
+  const logo = document.querySelector('.logo')
+
+  // scroll
+  logo.addEventListener('click', function(){
+    document.body.scrollTop = 0
+  })
   menus.forEach((i,idx) => i.addEventListener('click', function(){
     document.body.scrollTop = bgs[idx].getClientRects()[0].y + document.body.scrollTop - header.clientHeight
   }))
+  
 })
